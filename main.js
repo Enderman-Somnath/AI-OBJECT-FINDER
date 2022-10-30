cffc24955a3e36163730dc802c9db5ccfa65ec4a
Enderman-Somnath <91421328+Enderman-Somnath@users.noreply.github.com>
2022-10-30 17:00:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var status = "";
+var video = "";
+function preload(){
+    video = createVideo("video.mp4");
+    video.hide();
+}
+function setup() {
+ canvas = createCanvas(480, 380);
+ canvas.center();
+}
+function start(){
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML= "Status: Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Mode Has Successfully Loaded");
+    status = true;
+    video.loop();
+    video.speed(1.0);
+    video.volume(0);
+}
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
